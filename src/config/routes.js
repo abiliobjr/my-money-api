df2681f5c36cc0e48cfe7790822e966451986738
Abilio Bittencourt Junior <abilio.jr@live.com>
2019-01-27 21:08:40 -0200

    Acertando rota

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require('express');
 
 module.exports = function (server) {
     const router = express.Router();
     server.use('/api', router);
 
     //Rotas de ciclo de pagamento
-    const BillingCycle = require('../api/billingCycle/billingCycleService');
-    BillingCycle.register(router, 'billingCycles')
+    const BillingCycle = require('../api/billingCycle/billingCycleService.js');
+    BillingCycle.register(router, '/billingCycles')
 }
